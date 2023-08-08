795ecf489ae9d0ed69f0a6a91ed47feca5c41928
Mystery137 <iit2019086@iiita.ac.in>
2023-08-08 12:44:11 +0530

    Added few more buttons

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -13,6 +13,17 @@ export default function TextForm(props) {
         setText(newText);
     }
 
+    const handleCopy = ()=>{
+        var text = document.getElementById('myBox');
+        text.select();
+        navigator.clipboard.writeText(text.value);
+    }
+
+    const handleExtraSpaces = ()=>{
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(' '));
+    }
+
     const handleToClear = ()=>{
         let newText = "";
         setText(newText);
@@ -36,6 +47,8 @@ export default function TextForm(props) {
             <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
             <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to Lowercase</button>
             <button className="btn btn-primary mx-1" onClick={handleToClear}>Clear</button>
+            <button className="btn btn-primary mx-1" onClick={handleCopy}>Copy Text</button>
+            <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>Remove Extra Space</button>
         </div>
         <div className="container my-3">
             <h2>Your text summary</h2>
